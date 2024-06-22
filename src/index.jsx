c1fb10e382ca6031f7c38943e54d91aaf7e9755f
Nguyễn Hoàng Long <90452061+abcaxw@users.noreply.github.com>
2024-06-22 21:02:05 +0700

    update code index.jsx 22/06/2024 21:01

--- src/index.jsx
+++ src/index.jsx
@@ -14,6 +14,7 @@ ReactDOM.render(
       <BrowserRouter>
         <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
           <App />
+          
         </SnackbarProvider>
       </BrowserRouter>
     </Provider>
